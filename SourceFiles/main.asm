55a4f9e761fa12b184db0c1962a817674e403b74
jozfeal <163903053+jozfeal@users.noreply.github.com>
2026-02-18 14:57:02 -0500

    initial empty commit

---
+++ SourceFiles/main.asm
@@ -0,0 +1,13 @@
+.386
+.model flat,stdcall
+.stack 4096
+ExitProcess PROTO, dwExitCode:DWORD
+
+.code
+main PROC
+	mov eax,5 ; move 5 to the eax register
+	add eax,6 ; add 6 to the eax register
+
+INVOKE ExitProcess,0
+main ENDP
+END main
