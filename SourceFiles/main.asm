7a43eaba00dda42ef3222d347f8f62a73b542760
jozfeal <163903053+jozfeal@users.noreply.github.com>
2026-03-19 12:33:39 -0400

    added DrawManager.inc

--- SourceFiles/main.asm
+++ SourceFiles/main.asm
@@ -4,10 +4,9 @@
 INCLUDE Irvine32.inc
 INCLUDE Macros.inc
 INCLUDE AttackManager.inc
+INCLUDE DrawManager.inc
 
 ExitProcess PROTO, dwExitCode:DWORD
-ResetScreen PROTO
-Draw PROTO
 
 .data
 placeholderName BYTE "[Name]", 0
