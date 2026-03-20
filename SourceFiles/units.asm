2e688e2f38a10f2f13b1d0c5c1a35da5bdead5d7
jozfeal <163903053+jozfeal@users.noreply.github.com>
2026-03-20 14:42:07 -0400

    added initialized units for each party

--- SourceFiles/units.asm
+++ SourceFiles/units.asm
@@ -1,9 +1,33 @@
 .model flat,stdcall
-
 INCLUDE Units.inc
 
+; Macro to make a new ally unit, places it in the corresponding position
+mAllyUnit MACRO objName:REQ, name:REQ, role:REQ, pos:REQ
+	objName Unit <name, role>
+	.code
+	mov allies[pos * TYPE allies], OFFSET objName
+ENDM
+
+; Macro to make a new enemy unit, places it in the corresponding position
+mEnemyUnit MACRO objName:REQ, name:REQ, role:REQ, pos:REQ
+	objName Unit <name, role>
+	.code
+	mov enemies[pos * TYPE enemies], OFFSET objName
+ENDM
+
 .data
-allies Unit 3 DUP(<>)
-enemies Unit 3 DUP(<>)
+allies DWORD 3 DUP(?)
+enemies DWORD 3 DUP(?)
+
+.code
+InitializeUnits PROC
+	; Initializes constant characters to test with for each team
+	mAllyUnit ally1, "Edelgard", "Fighter", 0
+	mAllyUnit ally2, "Dimitri", "Soldier", 1
+	mAllyUnit ally3, "Claude", "Archer", 2
+	mEnemyUnit enemy1, "Jeritza", "Paladin", 0
+	mEnemyUnit enemy2, "Rhea", "Dragon", 1
+	mEnemyUnit enemy3, "Nemesis", "Liberator", 2
+InitializeUnits ENDP
 
 END
