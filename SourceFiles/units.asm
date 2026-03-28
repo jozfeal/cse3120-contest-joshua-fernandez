a2f28491f2f4481e8bac8c3d0326f50749c4a62f
jozfeal <163903053+jozfeal@users.noreply.github.com>
2026-03-28 19:50:28 -0400

    UpdateHealth finds correct place for health in unit display box

--- SourceFiles/units.asm
+++ SourceFiles/units.asm
@@ -1,6 +1,8 @@
 ;.model flat,stdcall
 INCLUDE Irvine32.inc
 INCLUDE Units.inc
+INCLUDE Definitions.inc
+INCLUDE Macros.inc
 
 .data
 allies UNIT 3 DUP (<>)
@@ -20,13 +22,35 @@ InitializeUnits PROC
 InitializeUnits ENDP
 
 ; ------------------------------
-UpdateHealth PROC USES eax, unitOffset:DWORD, change:DWORD
+UpdateHealth PROC USES eax edx, unitOffset:DWORD, change:DWORD
 ; Takes offset of the unit whose health is changing and the change in health
 ; Change can be negative or positive
 ; Returns number of choice selected in AL
 ; ------------------------------
-	mov eax, unitOffset
-	mov eax, change
+	.IF (change == 0)		; if there is no change, do nothing
+		ret
+	.ENDIF
+
+	mGetUnitField unitOffset, pos
+	mov dl, BYTE PTR [eax]	; get this unit's position in team
+
+	mov al, UNITCOL
+	mul dl					; calculate column start of unit box
+	add al, 8				; skip to curHealth amount in display
+	push eax				; save column
+	
+	mGetUnitField unitOFfset, team
+	mov al, BYTE PTR [eax]	; get's this unit team to determine row
+	.IF (al == ALLY)		; use ALLYROW for row if ally
+		pop eax
+		mGotoxy al, ALLYROW
+	.ELSEIF (al == ENEMY)	; use ENEMYROW for row if enemy
+		pop eax
+		mGotoxy al, ENEMYROW
+	.ELSE					; failsafe to keep stack working
+		pop eax
+	.ENDIF 
+
 	mov edx, ecx
 	dec edx
 	;mGetUnit ALLY, edx
