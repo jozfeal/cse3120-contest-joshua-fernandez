76a633410bf2ed5f771ec4bd4f2dd5eef5f7aa48
jozfeal <163903053+jozfeal@users.noreply.github.com>
2026-03-30 18:50:57 -0400

    RandomTarget will no longer return dead ally, fixes div by 0 runtime crash

--- SourceFiles/units.asm
+++ SourceFiles/units.asm
@@ -105,19 +105,25 @@ RandomTarget PROC USES edx ecx
 		mGetUnitField eax, curHealth
 		mov dl, BYTE PTR [eax]	; get their current health
 		
-		mGetUnit ALLY, ecx
-		mGetUnitField eax, maxHealth
-		movzx ax, BYTE PTR [eax]	; get their max health
+		.IF (dl != 0)			; only calculates weight if unit is alive
+			mGetUnit ALLY, ecx
+			mGetUnitField eax, maxHealth
+			movzx ax, BYTE PTR [eax]	; get their max health
 		
-		div dl					; divide maxHealth/curHealth
-		push eax				; store division result
-		movzx ax, al			; get quotient, lower health = higher weight = higher chance to be targeted
-		mov dx, 10
-		mul dx					; multiply quotient by 10, giving it a heavier weight than remainder
-		pop edx
-		movzx dx, dh			; get remainder into dx
-		add ax, dx				; add remainder to weight
-		mov weight[ecx * 2], ax		; store as weight for this unit
+			div dl					; divide maxHealth/curHealth
+			push eax				; store division result
+			movzx ax, al			; get quotient, lower health = higher weight = higher chance to be targeted
+			mov dx, 10
+			mul dx					; multiply quotient by 10, giving it a heavier weight than remainder
+			pop edx
+			movzx dx, dh			; get remainder into dx
+			add ax, dx				; add remainder to weight
+			mov weight[ecx * 2], ax		; store as weight for this unit
+		
+		.ELSE
+			mov ax, 0				; if unit is dead, they have a weight of zero
+			mov weight[ecx * 2], ax	; prevents unit from behing chosen
+		.ENDIF
 		
 		inc ecx
 	.ENDW
